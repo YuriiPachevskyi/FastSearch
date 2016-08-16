051541b842cd1ed9ca6ab8e4cff21a31b414bdae
Yurii Pachevskyi <yurii.pachevskyi@globallogic.com>
2016-08-16 19:35:33 +0300

    temp2

--- app/src/main/java/com/example/yuso/fastsearch/MenuFragment.java
+++ app/src/main/java/com/example/yuso/fastsearch/MenuFragment.java
@@ -3,44 +3,93 @@ package com.example.yuso.fastsearch;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 
-public class MenuFragment extends Fragment {
+public class MenuFragment extends Fragment implements SearchView.OnQueryTextListener {
 
     private  OnItemSelectedListener listener;
     private  ArrayAdapter<String> itemsAdapter;
-    private DataBase dataBase;
+    private DataBase   dataBase;
+    private ListView   mListView;
+    private SearchView mSearchView;
 
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if(context instanceof OnItemSelectedListener){      // context instanceof YourActivity
-            this.listener = (OnItemSelectedListener) context; // = (YourActivity) context
+        if(context instanceof OnItemSelectedListener) {
+            this.listener = (OnItemSelectedListener) context;
         } else {
             throw new ClassCastException(context.toString()
-                    + " must implement PizzaMenuFragment.OnItemSelectedListener");
+                    + " must implement MenuFragment.OnItemSelectedListener");
         }
-
-        Log.i("DEBUG", "Fragment - onAttach()");
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        dataBase = new DataBase((Context) listener);
-
+        dataBase     = new DataBase((Context) listener);
         itemsAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, dataBase.getRTMList());
-
-        Log.i("DEBUG", "Fragment - onCreate()");
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_menu, parent, false);
+
+        mListView   = (ListView) view.findViewById(R.id.list_view);
+        mSearchView = (SearchView) view.findViewById(R.id.search_view);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        ListView lvItems = (ListView) view.findViewById(R.id.list_view);
+        lvItems.setAdapter(itemsAdapter);
+        mListView.setTextFilterEnabled(true);
+        setupSearchView();
+
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                listener.onItemSelected((String) parent.getAdapter().getItem(position));
+            }
+        });
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (TextUtils.isEmpty(newText)) {
+            mListView.clearTextFilter();
+        } else {
+            mListView.setFilterText(newText.toString());
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    private void setupSearchView() {
+//        mSearchView.setIconifiedByDefault(false);
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setSubmitButtonEnabled(false);
+        mSearchView.setQueryHint("Search");
+    }
 
     public interface OnItemSelectedListener {
-        // This can be any number of events to be sent to the activity
-//        void onPizzaItemSelected(int position);
+        void onItemSelected(String str);
     }
 }
--- app/src/main/java/com/example/yuso/fastsearch/StartUpActivity.java
+++ app/src/main/java/com/example/yuso/fastsearch/StartUpActivity.java
@@ -1,7 +1,9 @@
 package com.example.yuso.fastsearch;
 
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
@@ -19,7 +21,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class StartUpActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+public class StartUpActivity extends AppCompatActivity
+        implements /*SearchView.OnQueryTextListener,*/ MenuFragment.OnItemSelectedListener {
     private static final String TAG = "MainActivity";
     private SearchView mSearchView;
     private ListView mListView;
@@ -32,116 +35,132 @@ public class StartUpActivity extends AppCompatActivity implements SearchView.OnQ
     private TextView motorNameRTMText;
     private TextView motorLocationText;
 
+    int orientation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.searchview_filter);
+        setContentView(R.layout.activity_main);
 
-        try {
-            DataBaseCopyist copyist = new DataBaseCopyist(getApplicationContext());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        dataBase = new DataBase(getApplicationContext());
+//        try {
+//            DataBaseCopyist copyist = new DataBaseCopyist(getApplicationContext());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        dataBase = new DataBase(getApplicationContext());
+//
+//        mSearchView = (SearchView) findViewById(R.id.search_view);
+//        mListView = (ListView) findViewById(R.id.list_view);
+//        mListView.setAdapter(new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1, dataBase.getRTMList()));
+//        mListView.setTextFilterEnabled(true);
+//        listFrameLayout = (FrameLayout) findViewById(R.id.search_frame_layout);
+//        displayFrameLayout = (FrameLayout) findViewById(R.id.display_frame_layout);
+//        motorConnectedText = (TextView) findViewById(R.id.motor_connection);
+//        motorNameText = (TextView) findViewById(R.id.motor_name);
+//        motorNameRTMText = (TextView) findViewById(R.id.motor_name_RTM);
+//        motorLocationText = (TextView) findViewById(R.id.motor_location);
 
-        mSearchView = (SearchView) findViewById(R.id.search_view);
-        mListView = (ListView) findViewById(R.id.list_view);
-        mListView.setAdapter(new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, dataBase.getRTMList()));
-        mListView.setTextFilterEnabled(true);
-        listFrameLayout = (FrameLayout) findViewById(R.id.search_frame_layout);
-        displayFrameLayout = (FrameLayout) findViewById(R.id.display_frame_layout);
-        motorConnectedText = (TextView) findViewById(R.id.motor_connection);
-        motorNameText = (TextView) findViewById(R.id.motor_name);
-        motorNameRTMText = (TextView) findViewById(R.id.motor_name_RTM);
-        motorLocationText = (TextView) findViewById(R.id.motor_location);
+//        setupSearchView();
+//        setOnItemClickListener();
+        orientation = getResources().getConfiguration().orientation;
 
-        setupSearchView();
-        setOnItemClickListener();
-    }
+        // PORTRAIT
+        if(orientation == Configuration.ORIENTATION_PORTRAIT){
+            if (savedInstanceState == null) {
+                // Instance of first fragment
+                MenuFragment firstFragment = new MenuFragment();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main_activitytest, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        listFrameLayout.setVisibility(View.VISIBLE);
-        displayFrameLayout.setVisibility(View.GONE);
-
-        switch (id) {
-            case (R.id.search_by_rtm):
-                stateFlag = STATE_LIST.NAME_RTM;
-                mListView.setAdapter(new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1, dataBase.getRTMList()));
-                return true;
-            case (R.id.search_by_connection):
-                stateFlag = STATE_LIST.CONNECTION;
-                mListView.setAdapter(new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1, dataBase.getConnectedList()));
-                return true;
-            case (R.id.search_by_name):
-                stateFlag = STATE_LIST.NAME;
-                mListView.setAdapter(new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1, dataBase.getNameList()));
-                return true;
-            case (R.id.search_by_location):
-                stateFlag = STATE_LIST.LOCATION;
-                mListView.setAdapter(new ArrayAdapter<String>(this,
-                        android.R.layout.simple_list_item_1, dataBase.getLocationList()));
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (listFrameLayout.getVisibility() == View.VISIBLE) {
-            this.finish();
-        } else {
-            listFrameLayout.setVisibility(View.VISIBLE);
-            displayFrameLayout.setVisibility(View.GONE);
-        }
-    }
-
-    public void setOnItemClickListener() {
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                String value = (String) arg0.getAdapter().getItem(arg2);
-                List<String> resultList = new ArrayList<String>();;
-
-                if (stateFlag == STATE_LIST.NAME_RTM) {
-                    resultList = dataBase.getValuesListByRTM(value);
-                } else if (stateFlag == STATE_LIST.CONNECTION) {
-                    resultList = dataBase.getValuesListByConnection(value);
-                } else if (stateFlag == STATE_LIST.NAME) {
-                    resultList = dataBase.getValuesListByName(value);
-                } else if (stateFlag == STATE_LIST.LOCATION) {
-                    resultList = dataBase.getValuesListLocation(value);
-                }
-
-                motorConnectedText.setText(resultList.get(0));
-                motorNameText.setText(resultList.get(1));
-                motorNameRTMText.setText(resultList.get(2));
-                motorLocationText.setText(resultList.get(3));
-                listFrameLayout.setVisibility(View.GONE);
-                displayFrameLayout.setVisibility(View.VISIBLE);
+                // Add Fragment to FrameLayout (flContainer), using FragmentManager
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();// begin  FragmentTransaction
+                ft.add(R.id.fragment_container, firstFragment);                                // add    Fragment
+                ft.commit();                                                            // commit FragmentTransaction
             }
-        });
+        }
     }
 
-    private void setupSearchView() {
-        mSearchView.setIconifiedByDefault(false);
-        mSearchView.setOnQueryTextListener(this);
-        mSearchView.setSubmitButtonEnabled(false);
-        mSearchView.setQueryHint("Search");
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main_activitytest, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        listFrameLayout.setVisibility(View.VISIBLE);
+//        displayFrameLayout.setVisibility(View.GONE);
+//
+//        switch (id) {
+//            case (R.id.search_by_rtm):
+//                stateFlag = STATE_LIST.NAME_RTM;
+//                mListView.setAdapter(new ArrayAdapter<String>(this,
+//                        android.R.layout.simple_list_item_1, dataBase.getRTMList()));
+//                return true;
+//            case (R.id.search_by_connection):
+//                stateFlag = STATE_LIST.CONNECTION;
+//                mListView.setAdapter(new ArrayAdapter<String>(this,
+//                        android.R.layout.simple_list_item_1, dataBase.getConnectedList()));
+//                return true;
+//            case (R.id.search_by_name):
+//                stateFlag = STATE_LIST.NAME;
+//                mListView.setAdapter(new ArrayAdapter<String>(this,
+//                        android.R.layout.simple_list_item_1, dataBase.getNameList()));
+//                return true;
+//            case (R.id.search_by_location):
+//                stateFlag = STATE_LIST.LOCATION;
+//                mListView.setAdapter(new ArrayAdapter<String>(this,
+//                        android.R.layout.simple_list_item_1, dataBase.getLocationList()));
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        if (listFrameLayout.getVisibility() == View.VISIBLE) {
+//            this.finish();
+//        } else {
+//            listFrameLayout.setVisibility(View.VISIBLE);
+//            displayFrameLayout.setVisibility(View.GONE);
+//        }
+//    }
+
+//    public void setOnItemClickListener() {
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+//                String value = (String) arg0.getAdapter().getItem(arg2);
+//                List<String> resultList = new ArrayList<String>();;
+//
+//                if (stateFlag == STATE_LIST.NAME_RTM) {
+//                    resultList = dataBase.getValuesListByRTM(value);
+//                } else if (stateFlag == STATE_LIST.CONNECTION) {
+//                    resultList = dataBase.getValuesListByConnection(value);
+//                } else if (stateFlag == STATE_LIST.NAME) {
+//                    resultList = dataBase.getValuesListByName(value);
+//                } else if (stateFlag == STATE_LIST.LOCATION) {
+//                    resultList = dataBase.getValuesListLocation(value);
+//                }
+//
+//                motorConnectedText.setText(resultList.get(0));
+//                motorNameText.setText(resultList.get(1));
+//                motorNameRTMText.setText(resultList.get(2));
+//                motorLocationText.setText(resultList.get(3));
+//                listFrameLayout.setVisibility(View.GONE);
+//                displayFrameLayout.setVisibility(View.VISIBLE);
+//            }
+//        });
+//    }
+
+//    private void setupSearchView() {
+//        mSearchView.setIconifiedByDefault(false);
+//        mSearchView.setOnQueryTextListener(this);
+//        mSearchView.setSubmitButtonEnabled(false);
+//        mSearchView.setQueryHint("Search");
+//    }
 
     public boolean onQueryTextChange(String newText) {
         if (TextUtils.isEmpty(newText)) {
@@ -154,6 +173,11 @@ public class StartUpActivity extends AppCompatActivity implements SearchView.OnQ
 
     public boolean onQueryTextSubmit(String query) {
         return false;
+    }
+
+    @Override
+    public void onItemSelected(String str) {
+        Log.e(TAG, str);
     }
 
     public static enum STATE_LIST {
